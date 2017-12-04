f3d9b21fdd18f38e7332cafd7c790510b5392af6
lijianan <lijianan@hujiang.com>
2017-12-04 17:52:12 +0800

    delete: 删除多余文件

--- run.py
+++
@@ -1,8 +0,0 @@
-fs_stock()
-# fs_stock_area()
-# fs_stock_concept()
-# fs_stock_industry()
-
-# fs_transaction_d_all()
-
-# fs_transaction_5min_all()
--- web/run.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=flaskr
-export FLASK_DEBUG=true
-flask run
