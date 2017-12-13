78485f06ee25622b8f87619e35118a1c7aed7ef5
lijianan <lijianan@hujiang.com>
2017-12-13 14:53:51 +0800

    update: 完善全局信息柱状图视图逻辑

--- web/flaskr/main/views/data_views/view_overall_bar.py
+++ web/flaskr/main/views/data_views/view_overall_bar.py
@@ -5,29 +5,38 @@ from flask import render_template, make_response, request
 from . import gen_view_data, make_view_response
 from ... import main
 
+bar_type = lambda id, name: {'id': id, 'name': name}
+
+bar_type_list = [
+    bar_type('industry', '行业分布'),
+    bar_type('area', '地区分布'),
+    bar_type('concept', '概念分布'),
+]
+
 
 def resp(views):
-    resp = make_response(render_template('view_pages/page_overall_bar.html',views=views))
+    resp = make_response(render_template('view_pages/page_overall_bar.html', views=views, bar_type_list=bar_type_list))
     return resp
 
 
 @main.route('/views/view_overall_bar', methods=['POST'])
 def view_overall_bar():
     type = request.form.get('type')
-    return make_view_response(plot_overall_bar, type=type)
+    limit = request.form.get('limit')
+    return make_view_response(plot_overall_bar, type=type, limit=limit)
 
 
-def plot_overall_bar(type):
+def plot_overall_bar(kargs):
     '''
     绘制 股票交易记录数据 【每日】
     :param name:
     :return:
     '''
-    sql = "select count(*) as count,c_name from stock_industry group by c_name order by count desc limit 5"
-    df = tu.execute_sql(sql)
-    df.cumsum(0)
+    type = kargs['type']
+    limit = kargs['limit']
 
-    plot = df.plot(x='c_name', title="行业公司数量分布", kind='bar')
+    plot = gen_plot(type, limit)
+
     fig = plot.get_figure()
 
     plt.ylabel('公司数量')
@@ -37,4 +46,40 @@ def plot_overall_bar(type):
 
     data = gen_view_data(fig)
     error = ""
-    return data,error
+    return data, error
+
+
+def gen_plot(type, limit):
+    plot = gen_industry_plot(limit)
+
+    if type == 'industry':
+        plot = gen_industry_plot(limit)
+    elif type == 'area':
+        plot = gen_area_plot(limit)
+    elif type == 'concept':
+        plot = gen_concept_plot(limit)
+    return plot
+
+
+def gen_industry_plot(limit):
+    sql = "select count(*) as count,c_name from stock_industry group by c_name order by count desc limit %s" % (limit)
+    df = tu.execute_sql(sql)
+    df.cumsum(0)
+    plot = df.plot(x='c_name', title="公司行业分布", kind='bar')
+    return plot
+
+
+def gen_area_plot(limit):
+    sql = "select count(*) as count,area from stock_area group by area order by count desc limit %s" % (limit)
+    df = tu.execute_sql(sql)
+    df.cumsum(0)
+    plot = df.plot(x='area', title="公司地区分布", kind='bar')
+    return plot
+
+
+def gen_concept_plot(limit):
+    sql = "select count(*) as count,c_name from stock_concept group by c_name order by count desc limit %s" % (limit)
+    df = tu.execute_sql(sql)
+    df.cumsum(0)
+    plot = df.plot(x='c_name', title="公司概念分布", kind='bar')
+    return plot
--- web/flaskr/static/js/view_overall_bar.js
+++ web/flaskr/static/js/view_overall_bar.js
@@ -1,21 +1,49 @@
 $(function() {
     $('#bt_show').click(function(){
-       ajaxForView("industry");
+
+        var type = getCrtType();
+        var limit = getLimitCount();
+        if(type.length == 0) {
+            alert('请选择一种展示类型');
+            return;
+        }
+       ajaxForView(type,limit);
 
     });
 
     // 用户获取绘图结果的ajax请求
-    function ajaxForView(type){
+    function ajaxForView(type,limit){
         $.ajax({
             url:"/views/view_overall_bar",
             type:'post',
             data:{
-                  type:type
+                  type:type,
+                  limit:limit
                 },
             dataType:"html",
             success: function(data,status){
               $('#view').html(data);
             }
         });
+    }
+
+    // 获取当前选中的类型
+    function getCrtType(){
+        var prefix = 'or_'
+        var options = $('input[id^='+prefix+']');
+        var selectId = '';
+        options.each(function(){
+            if(this.checked){
+                var start = prefix.length;
+                var end = this.id.length;
+                selectId = this.id.substring(start,end);
+            }
+        });
+        return selectId;
+    }
+
+    // 获取数量限制
+    function getLimitCount() {
+        return $('#ip_count').val()
     }
 })
