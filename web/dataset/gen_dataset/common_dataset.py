6151ba620369633830b72ce96ee89df30916be43
lijianan <lijianan@hujiang.com>
2017-12-16 11:00:34 +0800

    fix: 修复数据集生成bug

--- web/dataset/gen_dataset/common_dataset.py
+++ web/dataset/gen_dataset/common_dataset.py
@@ -19,27 +19,33 @@ def gen_trans_d_dataset(name, start_date, end_date=strutils.todayStr(), offset_d
     print('-- end_date => %s ' % end_date)
     print('-- offset_day => %s ' % offset_day)
 
-    X = None
-    Y = None
+    X = pd.DataFrame()
+    Y = pd.DataFrame()
 
-    for code in tu.all_trans_d_code():
+    des = "开始时间%s,结束时间%s,日期间隔%s" % (start_date, end_date, offset_day)
+    dataset = DataSet(TO_TRANS_D, name, X, Y, des)
+
+    for index, code in enumerate(tu.all_trans_d_code()):
         df = tu.query_trans_d(code, start_date, end_date)
         # 由于涨跌幅是次日的  所以要对数据进行'错位' 并且'错位之后'对X剔除末尾行，对Y剔除首行
         x = df[:-offset_day]
         y = pd.DataFrame({'p_change': df['p_change']})[offset_day:]
 
-        if X is not None:
-            X.append(x)
+        if len(X) > 0:
+            X = X.append(x, ignore_index=True)
         else:
             X = x
 
-        if Y is not None:
-            Y.append(y)
+        if len(Y) > 0:
+            Y = Y.append(y, ignore_index=True)
         else:
             Y = y
 
-    des = "开始时间%s,结束时间%s,日期间隔%s" % (start_date, end_date, offset_day)
-    dataset = DataSet(TO_TRANS_D, name, X, Y, des)
+        # 为尽量保证数据不丢失 每100步 存一次
+        if index % 10 == 0:
+            print("---【%s】执行一次保存操作 x shape %s---" % (name, X.shape))
+            dataset.save()
+
     dataset.save()
 
     print('%s 数据集创建成功' % name)
